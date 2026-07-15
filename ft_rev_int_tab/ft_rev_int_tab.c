e32e3e84b3639eacae2b179ecdabc97e87cdcaf2
NI09 <echos16_pepper@icloud.com>
2026-07-15 19:09:41 +0200

    Update corrected function to swap

--- ft_rev_int_tab/ft_rev_int_tab.c
+++ ft_rev_int_tab/ft_rev_int_tab.c
@@ -2,41 +2,46 @@
 
 void ft_rev_int_tab(int *tab, int size)
 {
-	int	tab2[size];
 	int	i;
+	int	j;
+	int	swap;
 
 	i = 0;
-	while(i < size)
+	j = size - 1;
+	while (i < (size / 2))
 	{
-		tab2[i] = *(tab + (size - 1 - i));
+		swap = tab[i];
+		tab[i] = tab[j];
+		tab[j] = swap;
 		i++;
+		j--;
 	}
+
+}
+
+void	print_array(int *tab, int size)
+{
+	int	i;
+	
 	i = 0;
-	while(i < size)
+	while (i < size)
 	{
-		tab[i] = tab2[i];
+		printf("%d ", tab[i]);
 		i++;
 	}
+	printf("\n");
 }
 
 int	main(void)
 {
-	int	tab[20];
-	int	i;
+	int	tab[] = {1, 2, 3, 4, 5};
 	int	size;
-
-	size = 20;
-	i = 0;
-	while(i < size)
-	{
-		tab[i] = size - i;
-		i++;
-	}
+	
+	size =  5;
+	printf("Before: ");
+	print_array(tab, size);
 	ft_rev_int_tab(tab, size);
-	i = 0;
-	while (i < size)
-	{
-		printf("%d: %d\n", i + 1, tab[i]);
-		i++;
-	}	
+	printf("After: ");
+	print_array(tab, size);	
+	return (0);
 }
